73d086c340e8cc8967f43855060b2f5654f05f03
Vu Van Dung <me@joulev.dev>
2023-10-11 20:09:22 +0700

    fix select being 24px wider than necessary
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -50,8 +50,7 @@ export const SelectContent = forwardRef<
     <SelectPrimitive.Content sideOffset={sideOffset} position={position} ref={ref} asChild>
       <Card
         className={cn(
-          // "relative", // TODO
-          "z-50 min-w-[8rem] overflow-hidden rounded-full border p-3 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+          "z-50 min-w-[8rem] overflow-hidden rounded-full border p-0 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
           className,
         )}
         {...props}
@@ -59,7 +58,7 @@ export const SelectContent = forwardRef<
         <SelectPrimitive.Viewport
           className={cn(
             position === "popper" &&
-              "flex h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)] flex-col gap-1",
+              "flex h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)] flex-col gap-1 p-3",
           )}
         >
           {children}
