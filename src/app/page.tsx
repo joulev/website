de0294e43a0b92e06bd1d6b7e9cea7f5ab1b1163
Vu Van Dung <me@joulev.dev>
2023-10-12 10:41:18 +0700

    add metadata
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { ChevronRight, Github, Mail } from "lucide-react";
+import { type Metadata } from "next";
 
 import { LinkButton } from "~/components/ui/button";
 import { Card } from "~/components/ui/card";
@@ -166,3 +167,37 @@ export default function Page() {
     </main>
   );
 }
+
+const title = "Vu Van Dung";
+const description = "Software developer. I build things for the web.";
+const url = "https://joulev.dev";
+export const metadata: Metadata = {
+  metadataBase: new URL("https://joulev.dev"),
+  title,
+  description,
+  robots: { index: true, follow: true },
+  twitter: { card: "summary_large_image", creator: "@joulev_3" },
+  openGraph: {
+    title,
+    description,
+    url,
+    siteName: title,
+    images: [
+      {
+        url: "https://static.joulev.dev/og?title=Vu+Van+Dung&subtitle=Software+developer",
+        alt: title,
+        width: 1200,
+        height: 630,
+      },
+    ],
+    type: "website",
+  },
+  alternates: { canonical: url },
+  icons: {
+    icon: [
+      { url: "https://static.joulev.dev/images/favicon.svg", type: "image/svg+xml" },
+      { url: "https://static.joulev.dev/favicon.ico", type: "image/x-icon", sizes: "any" },
+    ],
+    apple: [{ url: "https://static.joulev.dev/images/apple-touch-icon.png", sizes: "180x180" }],
+  },
+};
