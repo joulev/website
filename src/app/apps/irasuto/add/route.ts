bccc0189ecfcd8c1c005540a70ff2264a8aa884f
Vu Van Dung <me@joulev.dev>
2023-10-16 10:50:35 +0700

    add /add route for irasuto
    Signed-off-by: Vu Van Dung <me@joulev.dev>

---
+++ src/app/apps/irasuto/add/route.ts
@@ -0,0 +1,136 @@
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { EmbedBuilder } from "@discordjs/builders";
+import type { RESTPostAPIWebhookWithTokenJSONBody } from "discord-api-types/v10";
+import { sql } from "drizzle-orm";
+import { revalidateTag } from "next/cache";
+import { getTweet as _getTweet } from "react-tweet/api";
+import { z } from "zod";
+
+import { env } from "~/env.mjs";
+import { db } from "~/lib/db";
+import { photos as photosSchema } from "~/lib/db/schema";
+import type { NewIrasutoPhoto } from "~/lib/db/schema";
+
+const schema = z.object({ password: z.literal(env.PASSWORD), url: z.string().url() });
+
+const S3 = new S3Client({
+  region: "auto",
+  endpoint: `https://${env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  credentials: { accessKeyId: env.R2_ACCESS_KEY, secretAccessKey: env.R2_SECRET_ACCESS_KEY },
+});
+
+function convertUrlToPhotoId(url: string) {
+  return url.replace("https://pbs.twimg.com/media/", "");
+}
+
+async function uploadPhoto(url: string, tweetUrl: string) {
+  const id = convertUrlToPhotoId(url);
+  const res = await fetch(url);
+  if (!res.ok) throw new Error(`Failed to download photo for id=${id} (${tweetUrl})`);
+
+  const arrayBuffer = await res.arrayBuffer();
+  const metadata = res.headers.get("content-type");
+  const buffer = Buffer.from(arrayBuffer);
+
+  const result = await S3.send(
+    new PutObjectCommand({
+      Bucket: "webapps-irasuto",
+      Key: `irasuto/${id}`,
+      Body: buffer,
+      ContentType: metadata ?? undefined,
+    }),
+  );
+  if (result.$metadata.httpStatusCode !== 200)
+    throw new Error(`Failed to upload photo for id=${id} (${tweetUrl})`);
+}
+
+async function getPhotos(id: string): Promise<NewIrasutoPhoto[] | null> {
+  const tweet = await _getTweet(id);
+  if (!tweet?.photos) return null;
+  return tweet.photos.map(photo => ({
+    url: photo.url,
+    width: photo.width,
+    height: photo.height,
+    tweetUrl: `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`,
+    authorName: tweet.user.name,
+    authorHandle: tweet.user.screen_name,
+    date: new Date(tweet.created_at),
+  }));
+}
+
+function buildEmbedFromPhoto(
+  photo: NewIrasutoPhoto,
+  totalCount: number,
+): RESTPostAPIWebhookWithTokenJSONBody {
+  return {
+    embeds: [
+      new EmbedBuilder()
+        .setURL(photo.tweetUrl)
+        .setFields([
+          {
+            name: "Author",
+            value: `[@${photo.authorHandle}](<https://twitter.com/${photo.authorHandle}>)`,
+            inline: true,
+          },
+          {
+            name: "Source",
+            value: `[Click here](<${photo.tweetUrl}>)`,
+            inline: true,
+          },
+          {
+            name: "Posted",
+            value: `<t:${Math.round((photo.date ?? new Date()).valueOf() / 1000)}:R>`,
+            inline: true,
+          },
+        ])
+        .setImage(photo.url)
+        .setFooter({ text: `Total: ${totalCount}` })
+        .setColor(0x8ec8f6)
+        .toJSON(),
+    ],
+  };
+}
+
+export async function POST(request: Request) {
+  const e = (msg: string) => new Error(msg);
+  try {
+    const url = new URL(schema.parse(await request.json()).url);
+    if (url.hostname !== "twitter.com" && url.hostname !== "x.com")
+      throw e(`Invalid hostname: ${url.hostname}`);
+
+    if (url.hostname === "x.com") url.hostname = "twitter.com";
+
+    const id = url.pathname.split("/").at(-1);
+    if (!id) throw e(`Invalid pathname: ${url.pathname}`);
+
+    const photos = await getPhotos(id);
+    if (!photos) throw e(`No photos found for id=${id} (${url.href})`);
+
+    await Promise.all(
+      photos.map(async photo => {
+        await uploadPhoto(photo.url, photo.tweetUrl);
+        photo.url = `https://r2.irasuto.joulev.dev/irasuto/${convertUrlToPhotoId(photo.url)}`;
+
+        // We don't parallelise this because we don't want to have db items without photos
+        await db.insert(photosSchema).values(photo);
+
+        if (env.DISCORD_WEBHOOK) {
+          // This may be incorrect due to race conditions, but it's good enough
+          const query = await db.select({ count: sql<number>`COUNT(*)` }).from(photosSchema);
+          await fetch(env.DISCORD_WEBHOOK, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(buildEmbedFromPhoto(photo, query[0].count)),
+          });
+        }
+      }),
+    );
+
+    revalidateTag("photos");
+
+    return new Response("Ok!");
+  } catch (error) {
+    console.error(error);
+    return new Response("Invalid request", { status: 400 });
+  }
+}
--- src/env.mjs
+++ src/env.mjs
@@ -6,10 +6,17 @@ const YOUTUBE_VIDEO_ID_LENGTH = 11;
 
 export const env = createEnv({
   server: {
+    PASSWORD: z.string(),
     RESUME_URL: z.string().url(),
     GITHUB_TOKEN: z.string().startsWith("ghp_"),
     RECENT_FAVOURITE_SONG_ID: z.string().length(YOUTUBE_VIDEO_ID_LENGTH),
     DATABASE_URL: z.string().startsWith("postgres://"),
+
+    // irasuto
+    R2_ACCOUNT_ID: z.string().min(1),
+    R2_ACCESS_KEY: z.string().min(1),
+    R2_SECRET_ACCESS_KEY: z.string().min(1),
+    DISCORD_WEBHOOK: z.string().url().optional(),
   },
   experimental__runtimeEnv: {},
 });
