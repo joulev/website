816623d24e279c2595b11d82df7bc790e6aafbe4
Vu Van Dung <me@joulev.dev>
2023-10-29 15:28:26 +0700

    add some data to the sidebar
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- src/app/apps/anime/layout.tsx
+++ src/app/apps/anime/layout.tsx
@@ -1,14 +1,77 @@
+import {
+  Calendar,
+  CheckCircle,
+  Film,
+  PauseCircle,
+  PlayCircle,
+  Repeat,
+  Tv2,
+  XCircle,
+} from "lucide-react";
 import type { Metadata } from "next";
 
 import { Card } from "~/components/ui/card";
 
+import { getLists } from "./get-lists";
 import { Sidebar } from "./sidebar";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  const lists = await getLists();
   return (
     <main className="container max-w-screen-lg">
       <Card className="flex flex-col p-0 md:flex-row">
-        <Sidebar />
+        <Sidebar
+          items={[
+            {
+              content: "Watching",
+              icon: <PlayCircle />,
+              slug: "/watching",
+              count: lists.watching.length,
+            },
+            {
+              content: "Rewatching",
+              icon: <Repeat />,
+              slug: "/rewatching",
+              count: lists.rewatching.length,
+            },
+            {
+              content: "Completed TV",
+              icon: <Tv2 />,
+              slug: "/completed/tv",
+              count: lists.completedTV.length,
+            },
+            {
+              content: "Completed Movies",
+              icon: <Film />,
+              slug: "/completed/movies",
+              count: lists.completedMovies.length,
+            },
+            {
+              content: "Completed (others)",
+              icon: <CheckCircle />,
+              slug: "/completed/others",
+              count: lists.completedOthers.length,
+            },
+            {
+              content: "Paused",
+              icon: <PauseCircle />,
+              slug: "/paused",
+              count: lists.paused.length,
+            },
+            {
+              content: "Dropped",
+              icon: <XCircle />,
+              slug: "/dropped",
+              count: lists.dropped.length,
+            },
+            {
+              content: "Planning",
+              icon: <Calendar />,
+              slug: "/planning",
+              count: lists.planning.length,
+            },
+          ]}
+        />
         <div className="flex max-w-full flex-grow flex-col divide-y divide-separator overflow-x-auto">
           {children}
         </div>
--- src/app/apps/anime/sidebar.tsx
+++ src/app/apps/anime/sidebar.tsx
@@ -1,15 +1,5 @@
 "use client";
 
-import {
-  Calendar,
-  CheckCircle,
-  Film,
-  PauseCircle,
-  PlayCircle,
-  Repeat,
-  Tv2,
-  XCircle,
-} from "lucide-react";
 import { usePathname } from "next/navigation";
 
 import { Title } from "~/components/title";
@@ -22,36 +12,22 @@ import {
   SidebarSectionItems,
 } from "~/components/ui/sidebar";
 
-const items = [
-  { content: "Watching", icon: PlayCircle, slug: "/watching", count: 0 },
-  { content: "Rewatching", icon: Repeat, slug: "/rewatching", count: 0 },
-  { content: "Completed TV", icon: Tv2, slug: "/completed/tv", count: 0 },
-  {
-    content: "Completed Movies",
-    icon: Film,
-    slug: "/completed/movies",
-    count: 0,
-  },
-  {
-    content: "Completed (others)",
-    icon: CheckCircle,
-    slug: "/completed/others",
-    count: 0,
-  },
-  { content: "Paused", icon: PauseCircle, slug: "/paused", count: 0 },
-  { content: "Dropped", icon: XCircle, slug: "/dropped", count: 0 },
-  { content: "Planning", icon: Calendar, slug: "/planning", count: 0 },
-];
+interface Item {
+  content: React.ReactNode;
+  icon: React.ReactNode;
+  slug: string;
+  count: number;
+}
 
-function Navigation() {
+function Navigation({ items }: { items: Item[] }) {
   const pathname = usePathname();
   return (
     <SidebarSection>
       <SidebarSectionItems>
-        {items.map(({ content, icon: Icon, slug, count }) => (
+        {items.map(({ content, icon, slug, count }) => (
           <SidebarSectionItem key="slug" active={`/apps/anime${slug}` === pathname} asChild>
             <Link href={`/apps/anime${slug}`} unstyled>
-              <Icon />
+              {icon}
               <SidebarSectionItemName>{content}</SidebarSectionItemName>
               <SidebarSectionItemCounter>{count}</SidebarSectionItemCounter>
             </Link>
@@ -62,13 +38,13 @@ function Navigation() {
   );
 }
 
-export function Sidebar() {
+export function Sidebar({ items }: { items: Item[] }) {
   return (
     <div className="border-b border-separator bg-bg-darker md:w-64 md:shrink-0 md:border-r">
       <div className="p-6">
         <Title title="anime" subtitle="My anime list" />
       </div>
-      <Navigation />
+      <Navigation items={items} />
     </div>
   );
 }
