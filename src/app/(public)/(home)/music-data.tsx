fb82041f08f861d25bb46bddfbf3d4a4ec8c6a83
Vu Van Dung <me@joulev.dev>
2023-11-25 19:34:52 +0700

    update logic to fetch music data
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- src/app/(public)/(home)/music-data.tsx
+++ src/app/(public)/(home)/music-data.tsx
@@ -8,8 +8,9 @@ import { MetadataCard } from "./metadata-card";
 export async function MusicData() {
   const client = await new YTMusic().initialize();
   if (!client) throw new Error("invariant: client is falsy");
-  const data = await client.getSong(env.RECENT_FAVOURITE_SONG_ID);
-  const thumbnail = data.thumbnails
+  const song = await client.getPlaylistVideos(env.RECENT_FAVOURITE_PLAYLIST_ID).then(a => a.at(0));
+  if (!song) throw new Error("invariant: song is falsy");
+  const thumbnail = song.thumbnails
     .sort((a, b) => a.width - b.width)
     .find(value => value.width >= 72);
   return (
@@ -18,21 +19,21 @@ export async function MusicData() {
         // eslint-disable-next-line @next/next/no-img-element -- lh3.googleusercontent.com is too large for us to allow
         <img
           src={thumbnail?.url}
-          alt={data.name}
+          alt={song.name}
           className="h-18 w-18 rounded-[0.5rem] opacity-80"
         />
       }
       right={
         <>
-          <div className="truncate text-lg font-semibold">{data.name}</div>
+          <div className="truncate text-lg font-semibold">{song.name}</div>
           <div className="truncate text-sm text-text-secondary">
-            {data.artists.map(artist => artist.name).join(" & ")}
+            {song.artists.map(artist => artist.name).join(" & ")}
           </div>
         </>
       }
       title="Recent favourite"
       icon={PlayCircle}
-      href={`https://music.youtube.com/watch?v=${env.RECENT_FAVOURITE_SONG_ID}`}
+      href={`https://music.youtube.com/watch?v=${song.videoId}`}
     />
   );
 }
--- src/env.mjs
+++ src/env.mjs
@@ -2,14 +2,14 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
-const YOUTUBE_VIDEO_ID_LENGTH = 11;
+const YOUTUBE_PLAYLIST_ID_LENGTH = 34;
 
 export const env = createEnv({
   server: {
     PASSWORD: z.string(),
     RESUME_URL: z.string().url(),
     GITHUB_TOKEN: z.string().startsWith("ghp_"),
-    RECENT_FAVOURITE_SONG_ID: z.string().length(YOUTUBE_VIDEO_ID_LENGTH),
+    RECENT_FAVOURITE_PLAYLIST_ID: z.string().length(YOUTUBE_PLAYLIST_ID_LENGTH),
     DATABASE_URL: z.string().startsWith("postgres://"),
 
     // irasuto
