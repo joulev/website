2e4c91e056817102fe5214f2168ef1014c36c0b3
Vu Van Dung <me@joulev.dev>
2024-01-02 10:55:31 +0700

    more adjustments following testing
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- src/app/admin/manage/mood/page.tsx
+++ src/app/admin/manage/mood/page.tsx
@@ -14,8 +14,10 @@ import {
   DialogTrigger,
 } from "~/components/ui/dialog";
 import { List, ListContent, ListHeader, ListItem } from "~/components/ui/lists";
+import { ScrollArea } from "~/components/ui/scroll-area";
+import { cn } from "~/lib/cn";
 import { db } from "~/lib/db";
-import { dailyMoods } from "~/lib/db/schema";
+import { type Mood, dailyMoods } from "~/lib/db/schema";
 
 import { MoodForm } from "./mood-form";
 
@@ -24,6 +26,21 @@ const getHistoricalMoods = cache(() => db.select().from(dailyMoods), [], { tags:
 function renderDate(date: Date | string) {
   const formatter = new Intl.DateTimeFormat("en-US", { day: "2-digit", month: "long" });
   return formatter.format(new Date(date));
+}
+
+function getMoodClass(mood: Mood) {
+  switch (mood) {
+    case "horrible":
+      return "bg-red";
+    case "bad":
+      return "bg-orange";
+    case "meh":
+      return "bg-yellow";
+    case "good":
+      return "bg-green";
+    case "wonderful":
+      return "bg-mint";
+  }
 }
 
 export default async function Page() {
@@ -42,26 +59,43 @@ export default async function Page() {
                   <Dialog key={renderedDate}>
                     <DialogTrigger asChild>
                       <ListItem asChild>
-                        <button type="button" className="flex flex-row justify-between">
+                        <button
+                          type="button"
+                          className="flex flex-row items-center justify-between"
+                        >
                           <span>{renderedDate}</span>
-                          <span>{mood.mood}</span>
+                          <span
+                            className={cn(getMoodClass(mood.mood), "size-6 shrink-0 rounded-full")}
+                          />
                         </button>
                       </ListItem>
                     </DialogTrigger>
-                    <DialogContent className="sm:max-w-md">
-                      <DialogHeader>
-                        <DialogTitle>{renderedDate}</DialogTitle>
-                      </DialogHeader>
-                      <div className="prose max-w-full">
-                        <Markdown remarkPlugins={[remarkGfm]}>{mood.comment}</Markdown>
+                    <DialogContent className="p-0 sm:max-w-screen-sm">
+                      <div className="flex flex-col gap-6 py-6 *:px-6">
+                        <DialogHeader>
+                          <DialogTitle>
+                            <span className="inline-flex flex-row items-center gap-3">
+                              <span
+                                className={cn(
+                                  getMoodClass(mood.mood),
+                                  "size-6 shrink-0 rounded-full",
+                                )}
+                              />
+                              <span>{renderedDate}</span>
+                            </span>
+                          </DialogTitle>
+                        </DialogHeader>
+                        <ScrollArea className="max-h-[60vh] overflow-y-auto">
+                          <div className="prose max-w-full">
+                            <Markdown remarkPlugins={[remarkGfm]}>{mood.comment}</Markdown>
+                          </div>
+                        </ScrollArea>
+                        <DialogFooter>
+                          <DialogClose asChild>
+                            <Button className="w-full sm:w-auto">Cancel</Button>
+                          </DialogClose>
+                        </DialogFooter>
                       </div>
-                      <DialogFooter>
-                        <DialogClose asChild>
-                          <Button className="w-full sm:w-auto" variants={{ variant: "primary" }}>
-                            Cancel
-                          </Button>
-                        </DialogClose>
-                      </DialogFooter>
                     </DialogContent>
                   </Dialog>
                 );
