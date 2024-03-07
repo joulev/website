3e52c35a9ab251a8637a361c3eace73e44172f8d
Vu Van Dung <me@joulev.dev>
2024-03-08 02:37:34 +0800

    remove deprecated use of neonConfig.fetchConnectionCache
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,9 +1,7 @@
-import { neon, neonConfig } from "@neondatabase/serverless";
+import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
 import { env } from "~/env.mjs";
-
-neonConfig.fetchConnectionCache = true;
 
 const sql = neon<boolean, boolean>(env.DATABASE_URL);
 export const db = drizzle(sql);
