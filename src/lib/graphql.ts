22178c8f45d34e2b1590c77fdd4ced52422eb481
Vu Van Dung <me@joulev.dev>
2024-06-01 17:34:53 +0700

    test something
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- src/lib/graphql.ts
+++ src/lib/graphql.ts
@@ -1,8 +1,25 @@
 import { GraphQLClient } from "graphql-request";
+import { env } from "~/env.mjs";
 
 export function getClient(token?: string) {
   return new GraphQLClient("https://graphql.anilist.co", {
-    fetch,
+    fetch: async (url, init) => {
+      // Temporary "logging"
+      if (env.DISCORD_WEBHOOKS)
+        await fetch(env.DISCORD_WEBHOOKS[0], {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            content: `
+Fetching from AniList
+\`\`\`
+${init?.body}
+\`\`\`
+`,
+          }),
+        });
+      return fetch(url, init);
+    },
     headers: token ? { authorization: `Bearer ${token}` } : undefined,
   });
 }
