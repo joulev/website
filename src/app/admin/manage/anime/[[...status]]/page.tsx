976211e916220779616f51c852722414a0dd11b3
Vu Van Dung <me@joulev.dev>
2025-08-22 02:48:14 +0800

    i TRULY don't know why the HECK do i need to do this
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- src/app/admin/manage/anime/[[...status]]/page.tsx
+++ src/app/admin/manage/anime/[[...status]]/page.tsx
@@ -1,7 +1,11 @@
+import { notFound } from "next/navigation";
 import { PageClient } from "./page-client";
 
-export default async function Page({ params }: PageProps<"/admin/manage/anime/[[...status]]">) {
-  return <PageClient status={(await params).status} />;
+export default async function Page(props: PageProps<"/admin/manage/anime/[[...status]]">) {
+  const status = (await props.params).status || (await props.searchParams).status;
+  if (!status) return notFound();
+  const statusArray = Array.isArray(status) ? status : [status];
+  return <PageClient status={statusArray} />;
 }
 
 export function generateStaticParams(): Awaited<
